765a689d4ba9a9ecad0bebd2834ebd19f231d2d7
v-five <v-five@Dellia>
2014-04-14 11:07:31 +0200

    ...

--- routes/users.js
+++ routes/users.js
@@ -3,13 +3,13 @@
  */
 
 exports.list = function(db) {
-  return function(req, res) {
-    db.users.find(function(err, users) {
-	    if(err) res.send(err);
-	    else if(!users) res.send("No users.");
-	    else res.send(users);
-	  });
-  }
+	return function(req, res) {
+		db.users.find(function(err, users) {
+			if(err) res.send(err);
+			else if(!users) res.send("No users.");
+			else res.send(users);
+		});
+	}
 };
 
 
@@ -18,15 +18,14 @@ exports.list = function(db) {
  */
 
 exports.add = function(db) {
-
-  user = {email: "srirangan@gmail.com", password: "iLoveMongo", sex: "male"};
-  return function(req, res) {
-    db.users.save(user, function(err, saved) {
-      if(err) res.send(err);
-      else if(!saved) res.send(false);
-      else res.send(true);
-    });
-  }
+	return function(req, res) {
+		var user = {username: req.body.username, email: req.body.email, password: req.body.password, friends: [], content: []};
+		db.users.save(user, function(err, saved) {
+			if(err) res.send(err);
+			else if(!saved) res.send(false);
+			else res.send(true);
+		});
+	}
 };
 
 
@@ -35,19 +34,16 @@ exports.add = function(db) {
  */
 
 exports.delete = function(db) {
-
-  return function(req, res) {
-
-	var mongodb = require('mongodb');
-    userid = new mongodb.ObjectID(req.params.id);
-    user = {_id: userid};
-    console.log(user);
-    db.users.remove(user, function(err, removed) {
-      if(err) res.send(err);
-      else if(!removed) res.send(false);
-      else res.send(true);
-    });
-  }
+	return function(req, res) {
+		var mongodb = require('mongodb');
+		var userid = new mongodb.ObjectID(req.params.id);
+		var user = {_id: userid};
+		db.users.remove(user, function(err, removed) {
+			if(err) res.send(err);
+			else if(!removed) res.send(false);
+			else res.send(true);
+		});
+	}
 };
 
 
@@ -56,15 +52,15 @@ exports.delete = function(db) {
  */
 
 exports.update = function(db) {
-
-  userid = "534a8c051fa6211555b67628";
-  user = "{'_id', ObjectId("+userid+")}";
-  patch = "{$set: {password: 'testPass'}}";
-  return function(req, res) {
-    db.users.remove(user, patch, function(err, updated) {
-      if(err) res.send(err);
-      else if(!updated) res.send(false);
-      else res.send(true);
-    });
-  }
+	return function(req, res) {
+		var mongodb = require('mongodb');
+		var userid = new mongodb.ObjectID(req.params.id);
+		var user = {_id: userid};
+		var patch = {$set: {password: 'testPass'}};
+		db.users.remove(user, patch, function(err, updated) {
+			if(err) res.send(err);
+			else if(!updated) res.send(false);
+			else res.send(true);
+		});
+	}
 };
